05b331aff3d780ae3f0c6864c142f65624b968e8
AngryFennec <shellfix@gmail.com>
2025-11-09 21:09:46 +0300

    Add task

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,18 @@
 package main
 
-func main() {
+import (
+	"log"
+	"os"
 
+	"github.com/Yandex-Practicum/go1fl-sprint6-final/internal/server"
+)
+
+func main() {
+	logger := log.New(os.Stderr, "morse-converter", log.LstdFlags)
+	srv := server.CreateServer(logger)
+	log.Println("Running server on 8080")
+
+	if err := srv.Server.ListenAndServe(); err != nil {
+		logger.Fatalf("%s", err.Error())
+	}
 }
--- internal/server/server.go
+++ internal/server/server.go
@@ -1,1 +1,35 @@
 package server
+
+import (
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/Yandex-Practicum/go1fl-sprint6-final/internal/handlers"
+	"github.com/go-chi/chi/v5"
+)
+
+type Server struct {
+	Server *http.Server
+	Logger *log.Logger
+}
+
+func CreateServer(logger *log.Logger) *Server {
+	router := chi.NewRouter()
+	router.Get("/", handlers.IndexHandler)
+	router.Post("/upload", handlers.UploadHandler)
+
+	server := &http.Server{
+		Addr:         ":8080",
+		Handler:      router,
+		ErrorLog:     logger,
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 10 * time.Second,
+		IdleTimeout:  15 * time.Second,
+	}
+	return &Server{
+		Logger: logger,
+		Server: server,
+	}
+
+}
